c3746f5f688a5985567f252fba97c8046623f359
Ryan Christopher <ryanchristopher27@users.noreply.github.com>
2020-08-25 17:16:26 -0500

    personal projects
    personal project updates

--- Python/DiceRollSimulator.py
+++ Python/DiceRollSimulator.py
@@ -3,14 +3,113 @@
 
 import random
 
-selection = input("Would you like to roll the dice?\n[Yes || No]\t")
+def roll1() :
+    print("\n")
+    print("\t      000     ")
+    print("\t   000000     ")
+    print("\t   000000     ")
+    print("\t      000     ")
+    print("\t      000     ")
+    print("\t      000     ")
+    print("\t      000     ")
+    print("\t      000     ")
 
-print(selection)
+def roll2() :
+    print("\n")
+    print("\t   00000000   ")
+    print("\t 000     000  ")
+    print("\t        0000  ")
+    print("\t       0000   ")
+    print("\t    0000      ")
+    print("\t  0000        ")
+    print("\t 0000         ")
+    print("\t 00000000000  ")
 
-while (selection == "Yes") :    #Prints Dice Roll until user decides to stop
-    print("\nDice Roll: ")
-    print(random.randint(1, 6))     #Prints random number between 0 and 6
-    selection = input("\nWould you like to roll again?\n[Yes || No]\t")
+def roll3() :
+    print("\n")
+    print("\t   0000000    ")
+    print("\t 000     000  ")
+    print("\t         000  ")
+    print("\t       0000   ")
+    print("\t         000  ")
+    print("\t         000  ")
+    print("\t 000     000  ")
+    print("\t  000000000   ")
+
+def roll4() :
+    print("\n")
+    print("\t 000     000  ")
+    print("\t 000     000  ")
+    print("\t 000     000  ")
+    print("\t 000     000  ")
+    print("\t 00000000000  ")
+    print("\t         000  ")
+    print("\t         000  ")
+    print("\t         000  ")
+
+def roll5() :
+    print("\n")
+    print("\t 00000000000  ")
+    print("\t 000          ")
+    print("\t 000          ")
+    print("\t 0000000000   ")
+    print("\t         000  ")
+    print("\t         000  ")
+    print("\t 000     000  ")
+    print("\t  000000000   ")
+
+def roll6() :
+    print("\n")
+    print("\t  000000000   ")
+    print("\t 000     000  ")
+    print("\t 000          ")
+    print("\t 0000000000   ")
+    print("\t 000     000  ")
+    print("\t 000     000  ")
+    print("\t 000     000  ")
+    print("\t  000000000   ")
 
 
-print("\nThank you for playing the Dice Roll Simulator!")
+
+def main() :
+    # selection = input("Would you like to roll the dice?\n[Yes || No]\t")
+    test = False
+    while (test == False) :
+        selection = input("\nWould you like to roll again?\n[Yes || No]\t")
+        if (selection == "Yes" or selection == "No") :
+            test = True
+        else :
+            print("\n" + selection + " was not a valid response.")
+
+    print(selection)
+
+    while (selection == "Yes") :    #Prints Dice Roll until user decides to stop
+        print("\nDice Roll: ")
+        num = random.randint(1, 6)     #Prints random number between 0 and 6
+
+        if (num == 1) :
+            roll1()
+        elif (num == 2) :
+            roll2()
+        elif (num == 3) :
+            roll3()
+        elif (num == 4) :
+            roll4()
+        elif (num == 5) :
+            roll5()
+        else :
+            roll6()
+
+        test = False
+        while (test == False) :
+            selection = input("\nWould you like to roll again?\n[Yes || No]\t")
+            if (selection == "Yes" or selection == "No") :
+                test = True
+            else :
+                print("\n" + selection + " was not a valid response.")
+
+
+    print("\nThank you for playing the Dice Roll Simulator!")
+
+
+main()
--- Python/GuessTheNumber.py
+++ Python/GuessTheNumber.py
@@ -10,24 +10,19 @@ while (selection == "Yes") :    # Continues to play the game until the user spec
     guess = int(input("\nGuess a number between 0 and 100\n"))
 
     if (guess == num) :     # Checks if your first guess was the correct number
-        print("\nYou got it first try! The number was ")
-        print(num)
+        print("\nYou got it first try! The number was ", num)
     else :
         while (guess != num) :      # Repeats until the number is guessed
             if (guess > num) :
-                print("The number is less than:")
-                print(guess)
+                print("The number is less than:", guess)
                 guess = int(input("\nGuess again: \t"))
             elif (guess < num) :
-                print("The number is greater than:")
-                print(guess)
+                print("The number is greater than:", guess)
                 guess = int(input("\nGuess again: \t"))
             else :
-                print("\nYou guessed it! The number was:")
-                print(guess)
+                print("\nYou guessed it! The number was:", guess)
 
-    print("\nYou guessed it! The number was:")
-    print(guess)
+    print("\nYou guessed it! The number was:", guess)
 
     selection = input("\nWould you like to play again?\n[Yes || No]\t")
 
