b459e69149d1b5923f4241f1a3ab43b91efa7009
Ryan Christopher <ryandchristopher27@gmail.com>
2021-11-19 00:54:32 -0600

    Create lexiHelp.py

---
+++ Python/lexiHelp.py
@@ -0,0 +1,33 @@
+#Helping Lexi with school
+
+myreader = [["1","2","3","4"],["5","6","7","8"],["9","10","11","12"]]
+
+Production_Cost = []
+
+for i, row in enumerate(myreader):
+    Production_Cost[i] = row
+
+
+
+Band = []
+UsedBands = []
+
+
+count = 0
+for row in Band:
+    dcount = 0
+    bandcount = 0
+    if Band[count] not in UsedBands:
+        for i in data:
+            if Band[count] == data[dcount][2]:
+                pcsum = pcsum + float(data[dcount][3])
+                acsum = acsum + float(data[dcount][4])
+                bandcount+=1
+            dcount+=1
+        pcavg = pcsum/bandcount
+        acavg = acsum/bandcount
+
+        #all the print stuff
+        UsedBands.append(row)
+    count+=1
+    
